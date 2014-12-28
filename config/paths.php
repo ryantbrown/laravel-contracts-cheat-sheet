4bc3bf2bd796f0b3cce9090d3766f6c3007d830d
= <=>
2014-12-27 18:20:07 -0800

    added comments to paths config file

--- config/paths.php
+++ config/paths.php
@@ -2,12 +2,16 @@
 
 return [
 
+    // path to the contract files
     'contracts' => __DIR__.'/../vendor/illuminate/contracts',
 
+    // path where the static files will be generated
     'classes' => __DIR__.'/../dist/classes/',
 
+    // the path for compiled assets and templates
     'dist' => __DIR__.'/../dist/',
 
+    // the twig templates
     'templates' => __DIR__.'/../assets/templates/'
 
 ];
