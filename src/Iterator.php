15685ea5ec0966b28cc2997b32adac0577eb0d8d
= <=>
2014-12-27 21:36:36 -0800

    cleaned up Iteratora bit

--- src/Iterator.php
+++ src/Iterator.php
@@ -17,12 +17,8 @@ class Iterator {
     public function getFolders()
     {
         foreach($this->directories as $dir)
-        {
             if($dir->isDir() && $dir->getFilename() != '.git' && !$dir->isDot())
-            {
                 $folders[] = $dir->getFilename();
-            }
-        }
 
         return $folders;
     }
@@ -30,9 +26,7 @@ class Iterator {
     public function getFolderIterators(array $folders)
     {
         foreach($folders as $folder)
-        {
             $iterators[$folder] = new DirectoryIterator($this->path.'/'.$folder);
-        }
 
         return $iterators;
     }
@@ -40,10 +34,8 @@ class Iterator {
     public function getFolderClasses(DirectoryIterator $iterator)
     {
         foreach($iterator as $class)
-        {
             if(!$class->isDot()) $classes[] = $class->getFilename();
-        }
-
+        
         return $classes;
     }
 
